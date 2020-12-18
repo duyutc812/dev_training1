3c7df8910f633202282731421d1e7cb95699df16
duyutc812 <65037887+duyutc812@users.noreply.github.com>
2020-12-18 08:41:36 +0700

    algorithm_1
    algorithm_1

--- algorithm_1.py
+++ algorithm_1.py
@@ -1,18 +1,46 @@
+import random
 
-def min_step(x, y):
-    if x == y:
-        return 0
 
-    if x > y:
-        return x - y
+# def random_operator(S, lst_op, total_ref):
+#     while (True):
+#         S_expression = ''
+#         for i in range(0, len(S)):
+#             if i == len(S)-1:
+#                 S_expression = S_expression + S[i]
+#             else:
+#                 S_expression = S_expression + S[i] + random.choice(lst_op)
+#
+#         if eval(S_expression) == total_ref:
+#             return S_expression
 
-    if y % 2 == 1:
-        return 1 + min_step(x, y + 1)
-    else:
-        return 1 + min_step(x, y / 2)
+# S_expression = [['%s+' % i, '%s-' % i, '%s' % i] for i in S]
+#     # S_expression = S_expression[:-1]
+#     # print(L_a)
+#     # # for i0 in S_expression[0]:
+#     # #     for i1 in S_expression[1]:
+#     # #         for i2 in S_expression[2]:
+#     # #             for i3 in S_expression[3]:
+#     # #                 for i4 in S_expression[4]:
+#     # #                     for i5 in S_expression[5]:
+#     # #                         for i6 in S_expression[6]:
+#     # #                             for i7 in S_expression[7]:
+#     # #                                 if eval(i0+i1+i2+i3+i4+i5+i6+i7+S[-1]) == total_ref:
+#     # #                                     list_result.append(str(i0+i1+i2+i3+i4+i5+i6+i7+S[-1]))
+#     #
+#     # b = set(list_result)
+#     # print(*list(b), sep='\n')
+
+
+def list_expression(a, b, op):
+    if b:
+        for i in op:
+            k = a + i + b[0]
+            list_expression(k, b[1:], op)
+    elif eval(a) == 100:
+        print(a)
 
 
 if __name__ == '__main__':
-    x = 6
-    y = 8
-    print("min steps :", min_step(x, y))
+    S = str(123456789)
+    op = ['+', '-', '']
+    list_expression('1', '23456789', op)

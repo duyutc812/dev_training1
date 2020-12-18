3282b9ec8b23ba4365ece2850920cbc89d5ed24e
duyutc812 <65037887+duyutc812@users.noreply.github.com>
2020-12-18 08:43:33 +0700

    algorithm_2
    add algorithm_2

---
+++ algorithm.py
@@ -0,0 +1,17 @@
+def min_step(x, y):
+    if x == y:
+        return 0
+
+    if x > y:
+        return x - y
+
+    if y % 2 == 1:
+        return 1 + min_step(x, y + 1)
+    else:
+        return 1 + min_step(x, y / 2)
+
+
+if __name__ == '__main__':
+    x = 6
+    y = 8
+    print("min steps :", min_step(x, y))
